b204799f56eccf023ec15a4394927b4b9031746c
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-24 20:22:38 +0530

    Cars page added

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../assets/assets';
 
 const MyBookings = () => {
+
+  const [bookings, setBookings] = useState([]);
+
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData);
+  }
+
+  useEffect(() => {
+    fetchMyBookings();
+  } , [])
+
   return (
-    <div>MyBookings</div>
+    <div>
+      
+    </div>
   )
 }
 
