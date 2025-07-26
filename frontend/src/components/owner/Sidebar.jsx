7cc12655bdf78ccad97e62852c0c1fe81fe45227
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-26 23:12:58 +0530

    owner dashboard started

--- frontend/src/components/owner/Sidebar.jsx
+++ frontend/src/components/owner/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
         <div className='group relative'>
             <label htmlFor='image'>
                 <img src={image ? URL.createObjectURL(image) : user?.image || 
-                    "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=300"} alt=''/>
+                    "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=300"} alt='' className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto'/>
                 <input type='file' id='image' accept='image/*' hidden onChange={e => setImage(e.target.files[0])}/>
 
                 <div className='absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex
@@ -39,11 +39,13 @@ const Sidebar = () => {
 
         <div className='w-full'>
             {ownerMenuLinks.map((link, index) => (
-                <NavLink>
-                    <img src={linkpath === location.pathname ? link.coloredIcon : link.icon} alt='car icon'/>
+                <NavLink key={index} to={link.path} className={`relative flex items-center gap-2 w-full py-3 pl-4 first:mt-6 ${link.path === location.pathname ? 
+                    'bg-primary/10 text-primary' : 'text-gray-600'}`}>
+                    <img src={link.path === location.pathname ? link.coloredIcon : link.icon} alt='car icon'/>
                     <span className='max-md:hidden'>{link.name}</span>
-                    <div>
-                        
+                    <div className={`${link.path === location.pathname && 'bg-primary'} w-1.5 h-8
+                      rounded-l right-0 absolute`}>
+
                     </div>
                 </NavLink>
             ))}
