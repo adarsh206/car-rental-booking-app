45fb88ce6314662dd991b083457cbe110696ade9
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-27 12:33:50 +0530

    add car section started

--- frontend/src/pages/owner/AddCar.jsx
+++ frontend/src/pages/owner/AddCar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/owner/Title';
 
 const AddCar = () => {
+
+  const [image, setImage] = useState(null);
+  const [car, setCar] = useState({
+    brand: '',
+    model: '',
+    year: 0,
+    pricePerDay: 0,
+    category: '',
+    transmission: '',
+    fuel_type: '',
+    seating_capacity: 0,
+    location: '',
+    description: '',
+  })
+
   return (
-    <div>AddCar</div>
+    <div className='px-4 py-10 md:px-10 flex-1'>
+      <Title title='Add New Car' subTitle='Fill in details to list a new car for booking, including pricing, availability, 
+      and car specifications.'/>
+    </div>
   )
 }
 
