97f34ae7dea93ceaaf51efaff40073bc76df927b
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-28 20:10:05 +0530

    backend started

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => res.send("Server is running"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
