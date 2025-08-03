3969aec320df52ab8c3f4dd440a1bbe7f1f397cc
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-08-03 14:23:05 +0530

    booking cars integrated

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
-import { assets, dummyCarData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import CarCard from '../components/CarCard';
+import { useSearchParams } from 'react-router-dom';
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
 
 const Cars = () => {
 
+  // getting search params from url
+  const [searchParams] = useSearchParams();
+  const pickupLocation = searchParams.get('pickupLocation');
+  const pickupDate = searchParams.get('pickupDate');
+  const returnDate = searchParams.get('returnDate');
+
+  const {cars, axios} = useAppContext();
+
   const [input, setInput] = useState('');
+
+  const isSearchData = pickupLocation && pickupDate && returnDate
+  
+  const [filteredCars, setFilteredCars] = useState([]);
+
+  const searchCarAvailability = async () => {
+    const { data } = await axios.post('/api/bookings/check-availability', {location: pickupLocation, pickupDate, returnDate});
+    if(data.success){
+      setFilteredCars(data.availableCars);
+      if(data.availableCars.length === 0){
+        toast('No Cars Available')
+      }
+      return null
+    }
+  }
+
+  useEffect(() => {
+    isSearchData && searchCarAvailability()
+  }, [])
 
   return (
     <div>
@@ -21,9 +51,9 @@ const Cars = () => {
       </div>
 
       <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-10'>
-        <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>Showing {dummyCarData.length} Cars</p>
+        <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>Showing {filteredCars.length} Cars</p>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto'>
-          {dummyCarData.map((car, index) => (
+          {filteredCars.map((car, index) => (
             <div key={index}>
               <CarCard car={car}/>
             </div>
--- frontend/src/pages/owner/ManageBookings.jsx
+++ frontend/src/pages/owner/ManageBookings.jsx
@@ -1,14 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import { dummyMyBookingsData } from '../../assets/assets';
 import Title from '../../components/owner/Title';
+import { useAppContext } from '../../context/AppContext.jsx';
+import toast from 'react-hot-toast';
 
 const ManageBookings = () => {
 
-  const currency = import.meta.env.VITE_CURRENCY;
+  const { currency, axios } = useAppContext();
+
+  
   const [bookings, setBookings] = useState([]);
 
   const fetchOwnerBookings = async () => {
-    setBookings(dummyMyBookingsData);
+    
+    try {
+      const { data } = await axios.get('/api/bookings/owner');
+      data.success ? setBookings(data.bookings) : toast.error(data.message)
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+    const changeBookingStatus = async (bookingId, status) => {
+    
+    try {
+      const { data } = await axios.post('/api/bookings/change-status', {bookingId, status});
+      
+      if(data.success){
+        toast.success(data.message);
+        fetchOwnerBookings();
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
@@ -46,7 +72,8 @@ const ManageBookings = () => {
                   <td className='p-3'>{currency}{booking.price}</td>
                   <td className='p-3 max-md:hidden'><span className='bg-gray-100 px-3 py-1 rounded-full text-xs'>Offline</span></td>
                   <td className='p-3'>{booking.status === 'pending' ? (
-                    <select value={booking.status} className='px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none'>
+                    <select onChange={(e) => changeBookingStatus(booking._id, e.target.value)} value={booking.status} 
+                    className='px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none'>
                       <option value="pending">Pending</option>
                       <option value="cancelled">Cancelled</option>
                       <option value="confirmed">Confirmed</option>
